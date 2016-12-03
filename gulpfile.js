532a1a40ced621212f84ed18eebefb5d34f8d972
Richard Toner <toner.richard@gmail.com>
2016-12-03 21:56:29 +0000

    Setup a Browser Sync server as a default gulp task.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Start the Browsersync server and serve the website from it
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        browser: "google chrome"
+    });
+
+    // Reload if any changes to the source code is detected
+    // TODO: Make this more clever by restructuring stuff
+    gulp.watch('**/*').on('change', browserSync.reload);
+});
+
+// Serve the website as a default task
+gulp.task('default', ['serve']);
