084d6818dfc481fb9f44c9cae914dba61c4d1934
Richard Toner <toner.richard@gmail.com>
2016-12-03 19:33:25 +0000

    Added an alcohol step to the form and made the personal form steps dynamic to pick up all the people from the first form step.

--- js/rsvp.js
+++ js/rsvp.js
@@ -15,6 +15,10 @@ var formRoutes = {
     },
     '#rsvp-songs': {
         'previous': '#rsvp-dietaryRequirements',
+        'next': '#rsvp-alcohol'
+    },
+    '#rsvp-alcohol': {
+        'previous': '#rsvp-songs',
         'next': '#rsvp-messages'
     },
     '#rsvp-messages': {
@@ -80,6 +84,34 @@ function displayErrorMessage(message) {
     }, 3000);
 }
 
+function refreshPersonalFormSteps() {
+    // Clear the personal form steps
+    $('#rsvp-dietaryRequirements .form-group').remove();
+    $('#rsvp-songs .form-group').remove();
+    $('#rsvp-alcohol .checkbox').remove();
+
+    // Add fields for all confirmed people to the personal form steps
+    $('#rsvp-names input').each(function() {
+        var name = $(this).val();
+        var markup = '<div class="form-group">' +
+            '<label for="dietaryRequirements[' + name + ']">' + name + '</label>' +
+            '<input type="text" class="form-control" id="dietaryRequirements[' + name + ']" name="dietaryRequirements[' + name + ']"  placeholder="Dessicated coconut because it is icky">' +
+        '</div>';
+        $('#rsvp-dietaryRequirements').append(markup);
+        var markup = '<div class="form-group">' +
+            '<label for="songs[' + name + ']">' + name + '</label>' +
+            '<input type="text" class="form-control" id="songs[' + name + ']" name="songs[' + name + ']" placeholder="R. Kelly - Bump and Grind">' +
+        '</div>';
+        $('#rsvp-songs').append(markup);
+        var markup = '<div class="checkbox">' +
+            '<label>' +
+                '<input type="checkbox" name="alcohol[' + name + ']" value="yes" checked> ' + name +
+            '</label>' +
+        '</div>';
+        $('#rsvp-alcohol').append(markup);
+    });
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Event hookups
 ////////////////////////////////////////////////////////////////////////////////
@@ -92,13 +124,15 @@ $('.js-next').on('click', function() {
     } else {
         var newStepId = formRoutes[currentStepId]['next'];
     }
+
     // Validate the names
     if (currentStepId === '#rsvp-names') {
         if (!namesAreValid()) {
             return;
         }
+
+        refreshPersonalFormSteps();
     }
-    // TODO: Update fields for #rsvp-dietaryRequirements and #rsvp-songs
     gotoStep(newStepId);
 });
 
